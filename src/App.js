96e4f1c5c3b608c5b525eacf6e731fd68efd462f
DHINESHMOHAN17 <dhineshdevi18@gmail.com>
2024-06-28 21:24:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import data from "./data";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  function handleRemove(id) {
+    const newPeople = people.filter((person) => person.id !== id);
+    console.log(id);
+    setPeople(newPeople);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h1>{people.length} birthdays today</h1>
+          {people.map(({ id, name, age, image }) => (
+            <div key={id} className="inner">
+              <img src={image} alt={name} className="picture"/>
+              <div className="innercontent">
+                <h2>{name}</h2>
+                <p>{age} years old</p>
+                <button onClick={() => handleRemove(id) } className="innerbutton">delete</button>
+              </div>
+            </div>
+          ))}
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
   );
 }
 
